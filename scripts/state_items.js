4f012ae55894da5c751d26a1de29787aea8d02f6
Anna <castlesofplacebo@gmail.com>
2021-11-09 20:37:25 +0300

    chore: finished lab4

--- scripts/state_items.js
+++ scripts/state_items.js
@@ -1,19 +1,22 @@
-const sections = document.querySelectorAll("main section");
-const navLi = document.querySelectorAll("nav .container ul li");
-window.addEventListener("scroll", () => {
-    let current = "";
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if (pageYOffset >= sectionTop - sectionHeight / 3) {
-            current = section.getAttribute("id");
-        }
-    });
+$(window).on("load", () => {
+    const sections = document.querySelectorAll("main section");
+    const navLi = document.querySelectorAll("nav .container ul li");
 
-    navLi.forEach((li) => {
-        li.classList.remove("active");
-        if (li.classList.contains(current)) {
-            li.classList.add("active");
-        }
+    window.addEventListener("scroll", () => {
+        let current = "";
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLi.forEach((li) => {
+            li.classList.remove("active");
+            if (li.classList.contains(current)) {
+                li.classList.add("active");
+            }
+        });
     });
-});
+})
