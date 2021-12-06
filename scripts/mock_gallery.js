d4d478c09847cbf881a1e213ffc43d919c531427
Anna <castlesofplacebo@gmail.com>
2021-12-06 23:03:49 +0300

    chore: lab6 added

---
+++ scripts/mock_gallery.js
@@ -0,0 +1,34 @@
+function addPhoto(title, thumb, url) {
+    const img = document.createElement('img');
+    img.src = thumb;
+    img.alt = url;
+
+    document.querySelector('.main__gallery').appendChild(img);
+}
+
+function addPage(currentPage) {
+    document.querySelector('.main__loader').setAttribute('style', 'display: inline;');
+
+    fetch('https://jsonplaceholder.typicode.com/photos?_limit=8&_page=' + currentPage)
+        .then(response => response.json())
+        .then(data => {
+                for (let item of data) {
+                    addPhoto(item['title'], item['thumbnailUrl'], item['url'])
+                }
+            document.querySelector('.main__loader').setAttribute('style', 'display: none;');
+            document.querySelector('.fa').setAttribute('style', 'display: none;');
+            }
+        ).catch((error) => {
+        console.log(error);
+        document.querySelector('.fa').setAttribute('style', 'display: inline;')
+    });
+}
+
+let currentPage = 1;
+addPage(currentPage);
+
+document.querySelector('.main__more').addEventListener("click", function () {
+    currentPage++;
+
+    addPage(currentPage);
+})
